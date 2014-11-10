nkoep/dotfiles

commit 4846c78c2f66996846afdffca9716adeaef2e0c5

xmonad: integrate WindowBringer to jump to windows
Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

xmonad/.xmonad/xmonad.hs
@@ -13,9 +13,10 @@ import XMonad.Layout.Fullscreen
 import XMonad.Layout.Gaps (gaps)
 import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig (additionalKeys)
-import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.Run (safeSpawn, spawnPipe)
 import XMonad.Util.Cursor (setDefaultCursor)
 import XMonad.Actions.CycleWS
+import XMonad.Actions.WindowBringer (gotoMenuArgs)
 import XMonad.StackSet (greedyView, shift)
 
 -- Color definitions
@@ -71,37 +72,40 @@ prettyPrinter handle = defaultPP
 
 -- dmenu customizations
 dmenuOptions =
-    [ "-fn"
+    [ "-b"
+    , "-fn"
     , "'DejaVu Sans-8'"
     , "-nb"
-    , quoteString colorBg
+    , colorBg
     , "-nf"
-    , quoteString colorFg
+    , colorFg
     , "-sb"
-    , quoteString colorHighlight
+    , colorHighlight
     , "-h"
     , "30"
     ]
-    where quoteString s = printf "'%s'" s
-dmenu = intercalate " " $ "dmenu_run" : dmenuOptions
 
 -- Keybindings
 keybindings =
-    [ ((modMask', xK_p), spawn dmenu)
+    [ ((modMask', xK_p), safeSpawn "dmenu_run" dmenuOptions)
     , ((modMask', xK_n), spawn "nemo")
+    , ((smMask, xK_f), gotoMenuArgs dmenuOptions)
+    , ((modMask', xK_f), spawn "slock")
     , ((cmMask, xK_h), prevWS)
     , ((cmMask, xK_l), nextWS)
     , ((scmMask, xK_h), shiftToPrev)
     , ((scmMask, xK_l), shiftToNext)
     -- TODO: Add bindings for `prevScreen` and `nextScreen`.
-    -- TODO: Add bindings for media keys (use `parseKey`).
+    -- TODO: Add bindings for media keys, e.g., XF86MonBrightnessUp ->
+    --       xbacklight +10 (use `parseKey`).
     ]
     where modMask' = mod1Mask
-          cmMask = controlMask .|. modMask'
-          scmMask = shiftMask .|. cmMask
+          smMask   = shiftMask .|. modMask'
+          cmMask   = controlMask .|. modMask'
+          scmMask  = shiftMask .|. cmMask
 
 config' handle = defaultConfig
-    { normalBorderColor = colorBg
+    { normalBorderColor = colorFg
     , focusedBorderColor = colorHighlight
     , terminal = terminal'
     , layoutHook

commit 32e096bcc115aee0a88944e70ca515970c1de8f7

xmonad: fix keybinding error introduced in 4846c78
Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

xmonad/.xmonad/xmonad.hs
@@ -60,21 +60,22 @@ prettyPrinter handle = defaultPP
     { ppCurrent = xmobarColor colorHighlight "" . prependWSIndex
     , ppVisible = wrap "[" "]" . prependWSIndex
     , ppHidden = prependWSIndex
-    , ppHiddenNoWindows = prependWSIndex
+    , ppHiddenNoWindows = (++ "*") . prependWSIndex
     , ppTitle = shorten 50
     -- FIXME: Remove the "SmartSpacing 5" string added when using the
     --        smartSpacing layout modifier.
-    -- , ppLayout = const "bla"
+    , ppLayout = id
     , ppOutput = hPutStrLn handle
     }
     -- TODO: Add <action> strings to the name so xmobar can invoke xdotool to
     --       switch workspaces on label click.
+    -- TODO: Add a star (*) to workspaces that have windows.
     where prependWSIndex workspaceId =
             case (workspaceId `elemIndex` workspaces') of
                 Just idx -> show (idx+1) ++ ":" ++ workspaceId
                 Nothing  -> workspaceId
 
--- dmenu customizations
+-- dmenu customizations (requires the `dmenu-xft-height` AUR package)
 dmenuOptions =
     [ "-b"
     , "-fn"
@@ -94,7 +95,7 @@ keybindings =
     [ ((modMask', xK_p), safeSpawn "dmenu_run" dmenuOptions)
     , ((modMask', xK_n), spawn "nemo")
     , ((smMask, xK_f), gotoMenuArgs dmenuOptions)
-    , ((modMask', xK_f), spawn "slock")
+    , ((modMask', xK_F5), spawn "slock")
     , ((cmMask, xK_h), prevWS)
     , ((cmMask, xK_l), nextWS)
     , ((scmMask, xK_h), shiftToPrev)