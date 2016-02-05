27dbea7afc9f0f54f24337ff685f7f94242f4791
Niklas Koep <niklas.koep@gmail.com>
2016-02-05 09:38:33 +0100

    pystartup.py: import numpy modules
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- python/.pystartup.py
+++ python/.pystartup.py
@@ -1,7 +1,11 @@
 import sys
 import os
 
-if sys.version_info.major == 3:
+if sys.version_info.major == 2:
+    import numpy as np
+    import numpy.linalg as la
+    import numpy.random as rnd
+else:
     from importlib import import_module
 
     globals_ = globals()
