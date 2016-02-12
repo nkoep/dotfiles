b4e77339665aa10ba7eda1d1c458e6f5e1626dd7
Niklas Koep <niklas.koep@gmail.com>
2016-02-12 09:39:19 +0100

    pystartup: fix gi version warnings
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- python/.pystartup.py
+++ python/.pystartup.py
@@ -6,10 +6,19 @@ if sys.version_info.major == 2:
     import numpy.linalg as la
     import numpy.random as rnd
 else:
+    import gi
     from importlib import import_module
 
+    modules = [("GObject", None), ("GLib", None) , ("Gio", None),
+               ("Gtk", "3.0"), ("Gst", "1.0")]
+
     globals_ = globals()
-    for module in "GObject GLib Gio Gtk Gst".split():
+    for module, version in modules:
+        if version is not None:
+            try:
+                gi.require_version(module, version)
+            except ValueError:
+                continue
         try:
             globals_[module] = import_module("gi.repository.%s" % module)
         except ImportError:
