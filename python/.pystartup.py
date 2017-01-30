40db6aa2ecd7aac8a1252c168fd5941aa11df8a4
Niklas Koep <niklas.koep@gmail.com>
2017-01-30 09:53:22 +0100

    python: check availability of gi modules
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- python/.pystartup.py
+++ python/.pystartup.py
@@ -7,20 +7,24 @@ import numpy.random as rnd
 
 
 if sys.version_info.major == 3:
-    import gi
-    from importlib import import_module
+    try:
+        import gi
+    except ImportError:
+        pass
+    else:
+        from importlib import import_module
 
-    modules = [("GObject", None), ("GLib", None) , ("Gio", None),
-               ("Gtk", "3.0"), ("Gst", "1.0")]
+        modules = [("GObject", None), ("GLib", None) , ("Gio", None),
+                   ("Gtk", "3.0"), ("Gst", "1.0")]
 
-    globals_ = globals()
-    for module, version in modules:
-        if version is not None:
+        globals_ = globals()
+        for module, version in modules:
+            if version is not None:
+                try:
+                    gi.require_version(module, version)
+                except ValueError:
+                    continue
             try:
-                gi.require_version(module, version)
-            except ValueError:
-                continue
-        try:
-            globals_[module] = import_module("gi.repository.%s" % module)
-        except ImportError:
-            pass
+                globals_[module] = import_module("gi.repository.%s" % module)
+            except ImportError:
+                pass
