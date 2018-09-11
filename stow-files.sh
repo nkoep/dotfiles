e24a0a7d10ac84d1e227179e97fe28a7a505e62c
Niklas Koep <niklas.koep@gmail.com>
2018-09-11 08:38:09 +0200

    stow-files.sh: don't use bash brackets and...
    fix common issue with existing .zshrc file on first installs
    
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- stow-files.sh
+++ stow-files.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# Move .zshrc file that gets installed by oh-my-zsh.
+mv ~/.zshrc ~/.zshrc.bak
+
 # Install configs.
 mkdir -p ~/.config
 for d in $(tree --noreport -id); do
-  if [[ ! $d = "." ]]; then
+  if [ ! $d = "." ]; then
     stow -t ~/ $d
   fi
 done
