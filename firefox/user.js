82b29e156ab15e4f72ef7d3f494d056af8f26f0f
Niklas Koep <niklas.koep@gmail.com>
2022-08-26 22:34:56 +0200

    firefox: add basic user.js file
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

---
+++ firefox/user.js
@@ -0,0 +1,11 @@
+// Play nice with xmonad, i.e., disable menu bar toggling on Alt.
+user_pref("ui.key.menuAccessKeyFocuses", false);
+
+// Bye pocket.
+user_pref("extensions.pocket.enabled", false);
+
+// Remove page headers/footers when printing.
+user_pref("print.print_footerleft", "");
+user_pref("print.print_footerright", "");
+user_pref("print.print_headerleft", "");
+user_pref("print.print_headerright", "");
