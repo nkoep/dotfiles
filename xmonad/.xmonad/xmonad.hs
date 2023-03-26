83e486e04836af0958e52e45ffff9f84d21871cd
Niklas Koep <niklas.koep@gmail.com>
2023-03-26 13:27:13 +0200

    xmonad: use minimal config
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,139 +1,54 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
-
-import Control.Arrow (second)
-import Control.Monad (when, join)
-import System.Directory (doesFileExist, removeFile)
-import Data.List (elemIndex)
-import Data.Maybe (maybeToList)
-import qualified Graphics.X11.Xlib (Rectangle(..))
-import Codec.Binary.UTF8.String (decodeString)
+import Data.Maybe (fromMaybe)
+import System.Environment (lookupEnv)
 
 import XMonad
-import XMonad.Actions.CycleWS (nextWS, prevWS, shiftToPrev, shiftToNext)
+import XMonad.Actions.CycleWS (nextWS, prevWS)
 import XMonad.Actions.WindowBringer (gotoMenuArgs)
-import XMonad.Hooks.ManageDocks  -- TODO
-import XMonad.Hooks.ManageHelpers (isDialog, doCenterFloat)
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
-import XMonad.Layout.Fullscreen (fullscreenEventHook, fullscreenManageHook)
-import XMonad.Layout.LayoutModifier  -- TODO
-import XMonad.Layout.Gaps (gaps)
+import XMonad.Hooks.ManageDocks (avoidStruts, docks)
+import XMonad.Hooks.ManageHelpers (isDialog, doCenterFloat)
+import XMonad.Layout.Gaps (gaps, Direction2D(U, D, L, R))
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.Renamed (renamed, Rename (Replace))
+import XMonad.Layout.Spacing (spacing)
 import XMonad.Util.Cursor (setDefaultCursor)
-import XMonad.Util.EZConfig (additionalKeys, removeKeys)
-import XMonad.Util.Font (fi)
-import XMonad.Util.Run (safeSpawn, safeSpawnProg)
-
--- Color definitions
-colorHighlight = "#a51f1c"
-focusBorderColor = "green"
-colorFg = "#fafafa"
-colorBg = "#262729"
-
--- Miscellaneous settings
-terminal' = "kitty"
-borderWidth' = 2
-workspaces' =
-    [ "one"
-    , "two"
-    , "three"
-    , "four"
-    , "five"
-    , "six"
-    , "seven"
-    , "eight"
-    , "nine"
-    ]
-windowSpacing = 5
-
--- Layout modifier `spacing`, copied from XMonad.Layout.Spacing so we can
--- override the implementation for `modifierDescription`.
-data Spacing a = Spacing Int deriving (Show, Read)
-instance LayoutModifier Spacing a where
-    pureModifier (Spacing p) _ _ wrs =
-        (map (second $ shrinkRect p) wrs, Nothing)
-    modifierDescription (Spacing p) = ""
-
-shrinkRect p (Rectangle x y w h) =
-    Rectangle (x+fi p) (y+fi p) (w-2*fi p) (h-2*fi p)
-spacing p = ModifiedLayout (Spacing p)
+import XMonad.Util.EZConfig (removeKeys)
+import XMonad.Util.Run (safeSpawn)
 
 -- Layouts
 layouts = tiled ||| half ||| full
-    where gr                = (1 + (toRational(sqrt(5) :: Double))) / 2
-          renameLayout name = renamed [Replace name]
-          tiled             = renameLayout "T" $ Tall 1 (1 / 100) (1 / gr)
-          half              = renameLayout "H" $ Tall 1 (1 / 2) (1 / 2)
-          full              = renameLayout "F" $ Full
+    where goldenRatio = (1 + (toRational(sqrt(5) :: Double))) / 2
+          tiled       = Tall 1 (1 / 100) (1 / goldenRatio)
+          half        = Tall 1 (1 / 2) (1 / 2)
+          full        = Full
 
--- Float dialog windows
-floatHooks = isDialog --> doCenterFloat
+windowSpacing :: Int
+windowSpacing = 5
 
--- dmenu customizations (requires the `dmenu-xft-height` AUR package)
-dmenuOptions =
-    [ "-b"
-    , "-i"
-    , "-fn"
-    , "'DejaVu Sans-8'"
-    , "-nb"
-    , colorBg
-    , "-nf"
-    , colorFg
-    , "-sb"
-    , colorHighlight
-    , "-h"
-    , "30"
-    ]
-
--- Keybindings
-keybindings =
-    [ ((alt, xK_p), safeSpawn "dmenu_run" dmenuOptions)
-    , ((alt, xK_f), gotoMenuArgs dmenuOptions)
-    , ((alt, xK_F5), safeSpawnProg "slock")
-    , ((shiftAlt, xK_h), prevWS)
-    , ((shiftAlt, xK_l), nextWS)
-    , ((shiftControlAlt, xK_h), shiftToPrev)
-    , ((shiftControlAlt, xK_l), shiftToNext)
-    , ((alt, xK_Up), raiseBrightness)
-    , ((alt, xK_Down), lowerBrightness)
-    ]
-    where alt                = mod1Mask
-          shiftAlt           = shiftMask .|. alt
-          controlAlt         = controlMask .|. alt
-          shiftControlAlt    = shiftMask .|. controlAlt
-          brightnessStep     = "5%"
-          adjustBrightness o = safeSpawn "xbacklight" [o : brightnessStep]
-          raiseBrightness    = adjustBrightness '+'
-          lowerBrightness    = adjustBrightness '-'
-
-startupHook' = do
-    setDefaultCursor xC_left_ptr
-    safeSpawn "xbacklight" ["-set", "100%"]
-    spawn "feh --bg-fill ~/.wallpaper.jpg"
-
-config' = (ewmhFullscreen . ewmh . docks) def
-    { normalBorderColor = colorFg
-    , focusedBorderColor = focusBorderColor
-    , terminal = terminal'
+config' borderColor focusColor = (ewmhFullscreen . ewmh . docks) def
+    { normalBorderColor = fromMaybe "white" borderColor
+    , focusedBorderColor = fromMaybe "yellow" focusColor
+    , terminal = "kitty"
     , layoutHook
         = avoidStruts
-        $ gaps (zip [U, D, R, L] $ repeat windowSpacing)
+        $ gaps (zip [U, D, L, R] $ repeat windowSpacing)
         $ spacing windowSpacing
         $ smartBorders
         $ layouts
-    , manageHook
-        =   manageDocks
-        <+> floatHooks
-        <+> fullscreenManageHook
-    , workspaces = workspaces'
-    , borderWidth = borderWidth'
-    , startupHook = startupHook'
+    , startupHook = do setDefaultCursor xC_left_ptr
+    , manageHook = isDialog --> doCenterFloat
+    , borderWidth = 2
     , focusFollowsMouse = False
     , clickJustFocuses = False
-    } `additionalKeys` keybindings `removeKeys` restartCombo
-    where restartCombo = [(mod1Mask, xK_q)]
+    }
+    `removeKeys` [ (mod1Mask, xK_q)  -- Restart xmonad
+                 , (mod1Mask .|. shiftMask, xK_q)  -- Quit xmonad
+                 , (mod1Mask .|. shiftMask, xK_p)  -- dmenu
+                 ]
 
 main :: IO ()
 main = do
+    borderColor <- lookupEnv "X_THEME_FG"
+    focusColor <- lookupEnv "X_THEME_PRIMARY"
     safeSpawn "polybar-xmonad" []
-    xmonad $ config'
+    xmonad $ config' borderColor focusColor
