94ff443bc6c3d2a9bcd7e51685d57ab3b90af6e6
Niklas Koep <niklas.koep@gmail.com>
2024-08-25 10:13:56 +0200

    zsh: update history settings
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- zsh/.zplugins/history.zsh
+++ zsh/.zplugins/history.zsh
@@ -6,7 +6,8 @@
 # History command configuration
 setopt extended_history        # Record timestamp of command in HISTFILE.
 setopt hist_expire_dups_first  # Delete duplicates first when buffer exhausted.
-setopt hist_ignore_dups        # Ignore duplicate commands.
+setopt hist_find_no_dups       # Do not display a previously found event.
+setopt hist_ignore_all_dups    # Delete old event if new event is a duplicate.
 setopt hist_ignore_space       # Ignore commands that start with space.
 setopt hist_verify             # Show command with history expansion.
 setopt share_history           # Share command history data.
