cc1c08e44246cc9844657b18a9bf1d8becdb2179
Niklas Koep <niklas.koep@gmail.com>
2024-12-31 18:10:52 +0100

    firefox: remove smooth scroll settings
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- firefox/user.js
+++ firefox/user.js
@@ -10,25 +10,6 @@ user_pref("print.print_footerright", "");
 user_pref("print.print_headerleft", "");
 user_pref("print.print_headerright", "");
 
-// Smooth scrolling
-user_pref("general.smoothScroll", false);
-user_pref("general.smoothScroll.lines.durationMaxMS", 125);
-user_pref("general.smoothScroll.lines.durationMinMS", 125);
-user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 200);
-user_pref("general.smoothScroll.mouseWheel.durationMinMS", 100);
-user_pref("general.smoothScroll.msdPhysics.enabled", true);
-user_pref("general.smoothScroll.other.durationMaxMS", 125);
-user_pref("general.smoothScroll.other.durationMinMS", 125);
-user_pref("general.smoothScroll.pages", false);
-user_pref("general.smoothScroll.pages.durationMaxMS", 125);
-user_pref("general.smoothScroll.pages.durationMinMS", 125);
-user_pref("mousewheel.min_line_scroll_amount", 40);
-user_pref("mousewheel.system_scroll_override_on_root_content.enabled", true);
-user_pref("mousewheel.system_scroll_override_on_root_content.horizontal.factor", 175);
-user_pref("mousewheel.system_scroll_override_on_root_content.vertical.factor", 175);
-user_pref("toolkit.scrollbox.horizontalScrollDistance", 6);
-user_pref("toolkit.scrollbox.verticalScrollDistance", 2);
-
 // Force hardware decoding.
 user_pref("media.hardware-video-decoding.force-enabled", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
