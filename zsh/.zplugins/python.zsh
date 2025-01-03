f0b143090c400bd1d06ec6678ef9d9df1d126717
Niklas Koep <niklas.koep@gmail.com>
2025-01-03 22:31:46 +0100

    zsh: clean up venv activation hook
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- zsh/.zplugins/python.zsh
+++ zsh/.zplugins/python.zsh
@@ -1,13 +1,16 @@
 autoload -U add-zsh-hook
 
-LAST_PROJECT_DIR=""
+typeset -g VENV_DIR_NAME=.venv
+typeset -g PYTHON_VERSION_FILE=.python-version
 
-function find_python_version_file() {
+typeset -g current_project_dir=""
+
+function find_project_dir() {
   local dir="$PWD"
-  while [[ $dir != "/" ]]; do
-    if [[ -f "$dir/.python-version" ]]; then
+  while [[ -n $dir && $dir != "/" ]]; do
+    if [[ -f "$dir/$PYTHON_VERSION_FILE" ]]; then
       echo "$dir"
-      return
+      return 0
     fi
     dir=$(dirname "$dir")
   done
@@ -15,56 +18,97 @@ function find_python_version_file() {
 }
 
 function is_cached_dir_valid() {
-  [[ $PWD == "$LAST_PROJECT_DIR" ]]
+  [[ $PWD == "$current_project_dir" ]]
 }
 
 function validate_and_update_cache() {
-  local project_dir=$(find_python_version_file)
-  if [[ $project_dir != "$LAST_PROJECT_DIR" ]]; then
-    LAST_PROJECT_DIR="$project_dir"
+  local project_dir
+  if ! project_dir=$(find_project_dir); then
+    current_project_dir=""
+    return 1
   fi
-  echo "$LAST_PROJECT_DIR"
+
+  current_project_dir="${project_dir:A}"
+  return 0
 }
 
-function activate_or_create_venv() {
-  local project_dir="$1"
-  local venv_dir="${project_dir}/.venv"
-  local py_version_file="${project_dir}/.python-version"
+function activate_venv() {
+  local venv_dir="$1"
+  local activate_script="$venv_dir/bin/activate"
 
-  if [[ -f $py_version_file ]]; then
-    local required_version=$(cut -d. -f1,2 "$py_version_file")
-    if [[ -d $venv_dir ]]; then
-      local venv_python_version=$("$venv_dir/bin/python" --version 2>&1 | awk '{print $2}' | cut -d. -f1,2)
-      if [[ $required_version == "$venv_python_version" ]]; then
-        if [[ -z $VIRTUAL_ENV || $VIRTUAL_ENV != "$venv_dir" ]]; then
-          VIRTUAL_ENV_DISABLE_PROMPT=1 source "$venv_dir/bin/activate"
-        fi
-      else
-        echo "Warning: .venv Python version ($venv_python_version) does not match .python-version ($required_version)"
-      fi
-    else
-      uv venv --python "$required_version" "$venv_dir"
-      VIRTUAL_ENV_DISABLE_PROMPT=1 source "$venv_dir/bin/activate"
+  if [[ ! -f $activate_script ]]; then
+    echo "Error: Virtual environment activate script not found"
+    return 1
+  fi
+
+  if [[ -z $VIRTUAL_ENV || $VIRTUAL_ENV != "$venv_dir" ]]; then
+    VIRTUAL_ENV_DISABLE_PROMPT=1 source "$activate_script"
+  fi
+}
+
+function deactivate_venv() {
+  if [[ -n $VIRTUAL_ENV ]]; then
+    if ! type deactivate >/dev/null 2>&1; then
+      echo "Error: deactivate function not found"
+      return 1
     fi
-  fi
-}
-
-function auto_activate_venv() {
-  if ! command -v uv >/dev/null; then
-    return
-  fi
-
-  if is_cached_dir_valid; then
-    return
-  fi
-
-  local project_dir=$(validate_and_update_cache)
-  if [[ -n $project_dir ]]; then
-    activate_or_create_venv "$project_dir"
-  elif [[ -n $VIRTUAL_ENV ]]; then
     deactivate
   fi
 }
 
+function activate_or_create_venv() {
+ local project_dir="$1"
+ local venv_dir="${project_dir}/${VENV_DIR_NAME}"
+ local py_version_file="${project_dir}/${PYTHON_VERSION_FILE}"
+
+ [[ ! -f $py_version_file ]] && return 1
+
+ local required_version=$(cut -d. -f1,2 "$py_version_file")
+
+ if [[ -d $venv_dir ]]; then
+   local venv_python_version
+   local venv_python="$venv_dir/bin/python"
+   if ! venv_python_version=$("$venv_python" --version 2>&1 |
+       awk '{print $2}' | cut -d. -f1,2); then
+     echo "Error: Failed to determine Python version in virtual environment"
+     return 1
+   fi
+
+   if [[ $required_version == "$venv_python_version" ]]; then
+     activate_venv "$venv_dir"
+   else
+     echo "Warning: .venv Python version ($venv_python_version) does not" \
+          "match .python-version ($required_version)"
+     return 1
+   fi
+ else
+   if ! command -v uv >/dev/null; then
+     echo "Error: 'uv' command not found. Please install uv to create" \
+          "virtual environments."
+     return 1
+   fi
+
+   echo "Creating new virtual environment with Python $required_version..."
+   if ! uv venv --python "$required_version" "$venv_dir"; then
+     echo "Error: Failed to create virtual environment"
+     return 1
+   fi
+
+   activate_venv "$venv_dir"
+ fi
+}
+
+function auto_activate_venv() {
+  if is_cached_dir_valid && [[ -n $VIRTUAL_ENV ]]; then
+    return
+  fi
+
+  if ! validate_and_update_cache; then
+    deactivate_venv
+    return
+  fi
+
+  activate_or_create_venv "$current_project_dir"
+}
+
 add-zsh-hook precmd auto_activate_venv
-auto_activate_venv
