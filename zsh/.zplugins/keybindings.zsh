a42b46513c64f855c6c79568cd810570f5502d40
Niklas Koep <niklas.koep@deliveryhero.com>
2026-01-14 15:34:53 +0100

    Enable fzf completion
    Signed-off-by: Niklas Koep <niklas.koep@deliveryhero.com>

--- zsh/.zplugins/keybindings.zsh
+++ zsh/.zplugins/keybindings.zsh
@@ -5,7 +5,7 @@ autoload -U up-line-or-beginning-search
 zle -N up-line-or-beginning-search
 
 bindkey "^[[A" up-line-or-beginning-search
-if [[ -n "${terminfo[kcuu1]}" ]]; then
+if [[ -n ${terminfo[kcuu1]} ]]; then
   bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
 fi
 
@@ -14,17 +14,17 @@ autoload -U down-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
 bindkey "^[[B" down-line-or-beginning-search
-if [[ -n "${terminfo[kcud1]}" ]]; then
+if [[ -n ${terminfo[kcud1]} ]]; then
   bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
 fi
 
 # [Shift-Tab] - move through the completion menu backwards
-if [[ -n "${terminfo[kcbt]}" ]]; then
+if [[ -n ${terminfo[kcbt]} ]]; then
   bindkey "${terminfo[kcbt]}" reverse-menu-complete
 fi
 
 # [Delete] - delete forward
-if [[ -n "${terminfo[kdch1]}" ]]; then
+if [[ -n ${terminfo[kdch1]} ]]; then
   bindkey "${terminfo[kdch1]}" delete-char
 fi
 
@@ -38,3 +38,7 @@ bindkey "^[[1;5D" backward-word
 
 bindkey "^r" history-incremental-search-backward
 bindkey " " magic-space
+
+if command -v fzf >/dev/null; then
+  source <(fzf --zsh)
+fi
